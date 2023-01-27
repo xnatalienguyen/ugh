70bee5557581cc8155a6aa879bf210a71bbc5d5b
natalie <natalin6@uci.edu>
2023-01-26 20:12:20 -0800

    This is a commit statement

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/bin/git status
+/usr/bin/git pull
 /usr/bin/date > ~/ugh/version
 /usr/bin/git add ~/ugh/version ~/ugh/update_version.sh
 /usr/bin/git commit -m "This is a commit statement"
